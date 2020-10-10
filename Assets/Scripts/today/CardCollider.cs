83b609240fb0e162ecf6c1ef8e589c6d441746c1
minie12 <30361753+minie12@users.noreply.github.com>
2020-10-11 06:20:18 +0900

    edit on "Today" Scene

---
+++ Assets/Scripts/today/CardCollider.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardCollider : MonoBehaviour
+{
+    public GameObject totManager;
+    public GameObject darkBG;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnMouseUpAsButton()
+    {
+        this.GetComponent<RectTransform>().localPosition += Vector3.down * 150.0f;
+        bool doubleClicked = totManager.GetComponent<today_todayManager>().checkDoubleClick(this.gameObject.name);
+
+        if (doubleClicked) 
+        { 
+            Vector3 targetPoint = new Vector3(360, 600, 0);
+            this.gameObject.transform.parent = GameObject.Find("Canvas").transform;
+            StartCoroutine(CardSizeUp(this.transform, transform.position, targetPoint, 0.5f));
+        }
+
+    }
+
+    IEnumerator CardSizeUp(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+    {
+        float i = 0.0f;
+        float rate = 1.0f / time;
+        while (i < 1.0)
+        {
+            yield return new WaitForEndOfFrame();
+            i = i + (Time.deltaTime * rate);
+            thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+            if(thisTransform.localScale.x < 4f)
+                thisTransform.localScale += new Vector3(0.9f, 0.9f, 0);
+        }
+        darkBG.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/today/today_todayManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class today_todayManager : MonoBehaviour
+{
+    public UI_manager UImanager;
+    public Text hashcode;
+    public Image UIbackground;
+    public GameObject card;
+
+    // card related
+    string pickedCard;
+
+    void Awake()
+    {
+        /*
+        // Make More Cards
+        for (int i = 0; i < 10; i++)
+        {
+            //GameObject childObject = Instantiate(card, new Vector3(i * 10.0F, 0, 0), Quaternion.identity) as GameObject;
+            GameObject childObject = (GameObject)Instantiate(card);
+            childObject.transform.parent = card.transform;
+            childObject.GetComponent<RectTransform>().localPosition += Vector3.right * 5.0f;
+        }
+        */
+    }
+
+    void Start()
+    {
+        UIbackground.gameObject.SetActive(false);
+
+        UImanager.allUIoff();
+        //hashcode.GetComponent<Text>().text = user.hashcode.ToString();
+        hashcode.GetComponent<Text>().text = "JJJJJJ";
+    }
+    public void setting()
+    {
+        StartCoroutine(UImanager.UIon(UI_manager.UIstate.setting));
+    }
+
+    public void logout()
+    {
+        PlayerPrefs.DeleteAll();
+        UImanager.UIoff(UI_manager.UIstate.setting);
+        StartCoroutine("fadeoutAndLogin");
+    }
+
+    public IEnumerator fadeoutAndLogin()
+    {
+        float fade_time = 0.5f, start = 0.5f, end = 0.7f, time = 0f;
+        Color fade_color = UIbackground.color;
+
+        while (fade_color.a < end)
+        {
+            time += Time.deltaTime / fade_time;
+            fade_color.a = Mathf.Lerp(start, end, time);
+            UIbackground.color = fade_color;
+            yield return null;
+        }
+
+        SceneManager.LoadScene("login");
+    }
+
+    public bool checkDoubleClick(string tempCard)
+    {
+        if(pickedCard == tempCard)
+        {
+            // Tell the card to be picked up
+            return true;
+        }
+        else
+        {
+            if (pickedCard != null)
+            {
+                // put the previous card back into it's place
+                GameObject temp = GameObject.Find(pickedCard);
+                temp.GetComponent<RectTransform>().localPosition -= Vector3.down * 150.0f;
+            }
+
+            pickedCard = tempCard;
+            Debug.Log("Diff");
+            return false;
+        }
+    }
+
+    public void renewCardDeck()
+    {
+        if (pickedCard != null)
+        {
+            // put the previous card back into it's place
+            GameObject temp = GameObject.Find(pickedCard);
+            temp.GetComponent<RectTransform>().localPosition -= Vector3.down * 150.0f;
+
+            pickedCard = null;
+        }
+    }
+}
