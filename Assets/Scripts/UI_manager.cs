d8391231e2a3c32dc84877c1dd2cd99d45dd62f0
dahaelee <dah3132@gmail.com>
2020-10-10 15:12:03 +0900

    scene change, loginout

---
+++ Assets/Scripts/UI_manager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_manager : MonoBehaviour
+{
+    public Image[] UIs;
+    public static UIstate currentState = UIstate.None;
+
+    public enum UIstate
+    {
+        UIbackground,
+        setting,
+        None
+    }
+
+    public void allUIoff()
+    {
+        for (int i = 0; i < UIs.Length; i++)
+            UIs[i].gameObject.SetActive(false);
+
+        currentState = UIstate.None;
+    }
+
+    public IEnumerator UIon(UIstate uistate)
+    {
+        currentState = uistate;
+        UIs[(int)UIstate.UIbackground].gameObject.SetActive(true);
+        UIs[(int)uistate].gameObject.SetActive(true);
+        for (int i = 0; i < 5; i++)
+        {
+            UIs[(int)uistate].rectTransform.localScale = new Vector3((float)(0.95 + i * 0.01), (float)(0.95 + i * 0.01), (float)(0.95 + i * 0.01));
+            yield return 0;
+        }
+    }
+
+    public void UIoff(UIstate index)
+    {
+        UIs[(int)index].gameObject.SetActive(false);
+        currentState = UIstate.None;
+    }
+}
---
+++ Assets/Scripts/main_mainManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class main_mainManager : MonoBehaviour
+{
+    public UI_manager UImanager;
+    public Text hashcode;
+    public Image UIbackground;
+
+    private void Awake()
+    {
+        dataLoad();
+    }
+
+    void Start()
+    {
+        UIbackground.gameObject.SetActive(false);
+
+        UImanager.allUIoff();
+        hashcode.GetComponent<Text>().text = user.hashcode.ToString();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void setting()
+    {
+        StartCoroutine(UImanager.UIon(UI_manager.UIstate.setting));
+    }
+
+    public void logout()
+    {
+        PlayerPrefs.DeleteAll();
+        StartCoroutine("fadeoutAndLogin");
+    }
+
+    public IEnumerator fadeoutAndLogin()
+    {
+        float fade_time = 0.5f, start = 0.5f, end = 0.7f, time = 0f;
+        Color fade_color = UIbackground.color;
+
+        while (fade_color.a < end)
+        {
+            time += Time.deltaTime / fade_time;
+            fade_color.a = Mathf.Lerp(start, end, time);
+            UIbackground.color = fade_color;
+            yield return null;
+        }
+
+        SceneManager.LoadScene("login");
+    }
+
+    public void dataLoad()
+    {
+        user.hashcode = PlayerPrefs.GetString("hashcode", user.hashcode);
+    }
+}
